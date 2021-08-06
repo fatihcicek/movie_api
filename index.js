a58534dafa9e766f91795db54143f4127f925d0a
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-08-06 18:13:19 +0300

    Assignment 3
    Delete index.html, Update index.js, added express framework and morgan functionality

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+
+let movies = [    {
+        title: 'Something about Mary',
+        director: 'someone'
+    },
+    {
+        title: 'movie 2',
+        director: 'someone'
+    },
+    {
+        title: 'movie 3',
+        director: 'someone else'
+    }
+];
+
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(movies)
+});
+
+app.get('/', (req, res) => {
+    res.send('You have arrived to my Movie app!');
+});
+
+// error message
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something Broke!');
+});
+
+app.listen(8081, () => {
+    console.log('Your app is listening on port 8081.');
+});
