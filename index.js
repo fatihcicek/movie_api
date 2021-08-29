6f65abd9158a1ccbd736921289fee401dc0aebf1
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-08-29 22:23:02 +0300

    Update 2.10

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+mongoose.connect(process.env.CONNECTION_URI, {
    useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
